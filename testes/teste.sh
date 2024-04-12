3688a7812c94ea7c5fc58820566a94bb686f0cdb
Ângelo Garcia <fhzkngj@gmail.com>
2024-04-12 10:12:06 -0400

    Add error handling in test script

--- testes/teste.sh
+++ testes/teste.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+paralelo_caminho="../bin/paralelo"
+sequencial_caminho="../bin/sequencial"
+
+if [ ! -f "$paralelo_caminho"  ]; then
+    echo "Binário de solução paralela não encontrado: $paralelo_caminho"
+
+    exit 1
+fi
+
+if [ ! -f "$paralelo_caminho"  ]; then
+    echo "Binário de solução sequencial não encontrado: $sequencial_caminho"
+
+    exit 1
+fi
+
 for i in {1..3}; do
     echo "Testando paralelo $i"
 
-    time ../bin/paralelo | wc -l
+    time $paralelo_caminho | wc -l
 done
 
 for i in {1..3}; do
     echo "Testando sequencial $i"
 
-    time ../bin/sequencial | wc -l
+    time $sequencial_caminho | wc -l
 done
